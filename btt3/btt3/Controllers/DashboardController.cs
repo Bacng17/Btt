ba4054f0e1ad78b5dd802d97d8d6092ba61569d3
Bacng17 <bacng17@gmail.com>
2025-09-18 16:55:07 +0700

    Resolve merge conflict

---
+++ btt3/btt3/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btt3.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["LayoutType"] = "Admin";
+            return View();
+        }
+    }
+
+}
