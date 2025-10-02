91efaba059c15c7b384b0a2a786e67278d38d6f7
Bacng17 <bacng17@gmail.com>
2025-10-02 16:56:56 +0700

    Cập nhật code

--- Btt2.2/Btt2.2/obj/Debug/net8.0/Btt2.2.AssemblyInfo.cs
+++ Btt2.2/Btt2.2/obj/Debug/net8.0/Btt2.2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Btt2.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702f7255656a3fd4819a1fc689100fddcc34400f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Btt2.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Btt2.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ NetCoreMVCLAB5/NetCoreMVCLAB5/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCLAB5.Models;
+using System.Text.RegularExpressions;
+
+namespace NetCoreMVCLAB5.Controllers
+{
+    public class AccountController : Controller
+    {
+        private static List<Account> accounts = new List<Account>();
+
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                accounts.Add(account); 
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // Get : Account
+        public ActionResult Index()
+        {
+        //    List<Account> accounts = new List<Account>();
+            return View(accounts);
+        }
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult VerifyPhone(string phone)
+        {
+            Regex regex_IsPhone = new Regex(@"^((\([0-9]{3}\))|([0-9]{3}))[-. ]?([0-9]{3})[-. ]?([0-9]{4})$");
+            if (!regex_IsPhone.IsMatch(phone))
+            {
+                return Json($"Số điện thoại {phone} không đúng định dạng, VD: 0986421127 hoặc 098.421.1127");
+            }
+            return Json(true);
+        }
+    }
+}
